024453013f3ed8ce2fb68fe95436fb54379debfe
unknown <you@example.com>
2021-11-16 13:48:53 +0545

    deplyoing to payara|restapi with servlet

---
+++ src/main/java/com/nepalaya/studentmgmt/controller/StudentController.java
@@ -0,0 +1,58 @@
+package com.nepalaya.studentmgmt.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nepalaya.studentmgmt.model.Student;
+import com.nepalaya.studentmgmt.response.Response;
+import com.nepalaya.studentmgmt.service.StudentService;
+import com.nepalaya.studentmgmt.service.impl.StudentServiceImpl;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class StudentController extends Controller {
+
+    private final static StudentService studentService = new StudentServiceImpl();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Student student = objectMapper.readValue(inputStream, Student.class);
+        Response responseBody = studentService.add(student);
+        buildResponse(response, responseBody);
+    }
+
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            String[] split = request.getRequestURL().toString().split("/");
+            if (split[split.length - 1].matches("[1-9]\\d*")) {
+                Response responseBody = studentService.getById(Long.parseLong(split[split.length - 1]));
+                buildResponse(response, responseBody);
+            } else {
+                Response responseBody = studentService.getAll();
+                buildResponse(response, responseBody);
+            }
+        }
+
+    @Override
+        protected void doPut(HttpServletRequest req, HttpServletResponse response) throws IOException {
+            String[] split = req.getRequestURL().toString().split("/");
+            ServletInputStream inputStream = req.getInputStream();
+            ObjectMapper objectMapper = new ObjectMapper();
+            Student student = objectMapper.readValue(inputStream, Student.class);
+            student.setId(Long.parseLong(split[split.length - 1]));
+            Response responseBody = studentService.update(student);
+            buildResponse(response, responseBody);
+        }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse response) throws IOException {
+        String[] split = req.getRequestURL().toString().split("/");
+        System.out.println(split[0]);
+        Response responseBody = studentService.delete(Long.parseLong(split[split.length - 1]));
+        buildResponse(response, responseBody);
+    }
+
+}
---
+++ src/main/java/com/nepalaya/studentmgmt/util/JacksonUtil.java
@@ -0,0 +1,11 @@
+package com.nepalaya.studentmgmt.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JacksonUtil {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    public static String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(object);
+    }
+}
